0bd282c94a78305073e86f39adc0921ff817bcfc
prasadhonrao <honrao.prasad@gmail.com>
2024-03-01 09:22:15 +0000

    Boilerplate code cleanup

--- src/DevQA/DevQA/Program.cs
+++ src/DevQA/DevQA/Program.cs
@@ -18,7 +18,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
